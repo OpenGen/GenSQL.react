d879fb4d755225b23b14a5d4a1239fd9448a774f
Zane Shelby <zane@users.noreply.github.com>
2022-06-19 17:02:25 -0400

    fix: Miscellaneous fixes

---
+++ index.js
@@ -0,0 +1,5 @@
+export { Button } from "./lib/Button";
+export { Query } from "./lib/Query";
+export { Spinner } from "./lib/Spinner";
+export { Table } from "./lib/Table";
+export { TextArea } from "./lib/TextArea";
