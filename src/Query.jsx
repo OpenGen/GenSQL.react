82d109d71de6ff838e6ec7c4f33baf5294133009
Zane Shelby <zane@users.noreply.github.com>
2022-06-19 22:50:09 -0400

    feat: Miscellaneous enhancements

--- src/Query.jsx
+++ src/Query.jsx
@@ -1,23 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Button } from 'antd';
-import { Card } from 'antd';
-import { Input } from 'antd';
-import { Option } from 'antd';
-import { Select } from 'antd';
-import { Space } from 'antd';
-import { Table } from 'antd';
+import { Button, Card, Form, Input, Space, Table } from 'antd';
 
 const { TextArea } = Input;
-
-const select = (
-  <Select defaultValue=".com" className="select-after">
-    <Option value=".com">.com</Option>
-    <Option value=".jp">.jp</Option>
-    <Option value=".cn">.cn</Option>
-    <Option value=".org">.org</Option>
-  </Select>
-);
 
 export const Query = (props) => {
   const [query, setQuery] = React.useState();
@@ -35,13 +20,13 @@ export const Query = (props) => {
     buttonRef.current.focus();
     textAreaRef.current.blur();
   };
-  const queryButton = (<Button ref={buttonRef} onClick={execute}>Execute</Button>);
+  const queryButton = (<Button type="primary" ref={buttonRef} onClick={execute}>Execute</Button>);
 
   const reset = () => {
     setResult();
     setFocus(true);
   };
-  const resetButton = (<Button ref={buttonRef} onClick={reset}>Reset</Button>);
+  const resetButton = (<Button type="primary" ref={buttonRef} onClick={reset}>Reset</Button>);
 
   const button = (result !== undefined) ? resetButton : queryButton;
 
@@ -71,8 +56,10 @@ export const Query = (props) => {
         bordered
         columns={columns}
         dataSource={dataSource}
+        pagination={false}
         size="small"
         sortDirections={["ascend", "descend"]}
+        style={{ marginTop: 24 }}
         tableLayout="auto"
       />
     );
@@ -95,23 +82,23 @@ export const Query = (props) => {
 
   return (
     <Card>
-      <Space
-        direction="vertical"
-        size="middle"
-        style={{ display: "flex" }}
-      >
-        <TextArea
-          autoSize
-          defaultValue={query}
-          disabled={result !== undefined}
-          onChange={onChange}
-          onKeyPress={onKeyPress}
-          placeholder={props.placeholder}
-          ref={textAreaRef}
-        />
-        {button}
-        {table}
-      </Space>
+      <Form layout="horizontal">
+        <Form.Item>
+          <TextArea
+            autoSize
+            defaultValue={query}
+            disabled={result !== undefined}
+            onChange={onChange}
+            onKeyPress={onKeyPress}
+            placeholder={props.placeholder}
+            ref={textAreaRef}
+          />
+        </Form.Item>
+        <Form.Item style={{ marginBottom: 0}}>
+          {button}
+        </Form.Item>
+      </Form>
+      {table}
     </Card>
   );
 };
