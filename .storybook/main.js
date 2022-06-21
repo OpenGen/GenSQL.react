aa113b79fe4ed713d82e6bd88c7cdd8ecaa8b1b2
Zane Shelby <zane@users.noreply.github.com>
2022-06-21 14:34:00 -0400

    build: Remove Tailwind, PostCSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,15 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss')
-        }
-      }
-    }
   ],
+  "features": {
+    "postcss": false,
+  },
   "framework": "@storybook/react"
 }
