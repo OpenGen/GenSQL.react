aa057f60d7646ac5361c991f4c6cada0ba28ab00
Zane Shelby <zane@users.noreply.github.com>
2022-06-22 01:45:13 -0400

    feat(Editor): Add syntax highlighting

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,9 +1,93 @@
 import Highlight, { defaultProps } from "prism-react-renderer";
+import Prism from 'prismjs';
 import PropTypes from 'prop-types';
 import React, { useCallback, useRef } from "react";
-import github from 'prism-react-renderer/themes/github';
+import theme from './themes/nordLight';
 import { Code } from '@mantine/core';
 import { useEditable } from "use-editable";
+
+// https://prismjs.com/extending.html
+// https://github.com/PrismJS/prism/blob/master/components/prism-sql.js
+
+const builtins = [
+  "(APPROXIMATE\\s+)?MUTUAL\\s+INFORMATION\\s+OF",
+  "PROBABILITY(\\s+DENSITY)?\\s+OF",
+  "(CONDITIONED|CONSTRAINED)\\s+BY",
+];
+
+const keywords = [
+  "ALTER",
+  "AS",
+  "ASC",
+  "(CREATE|DROP)\\s+(TABLE|MODEL)",
+  "DESC",
+  "DISTINCT",
+  "EXISTS",
+  "FROM",
+  "GENERATE",
+  "GIVEN",
+  "GROUP\\s+BY",
+  "IF",
+  "INCORPORATE",
+  "(INNER|CROSS)\\s+JOIN",
+  "INSERT",
+  "INTO",
+  "IS",
+  "LIMIT",
+  "ON",
+  "ORDER\\s+BY",
+  "SELECT",
+  "SET",
+  "UNDER",
+  "UPDATE",
+  "VALUES",
+  "VAR",
+  "WHERE",
+  "WITH",
+];
+
+const functions = [
+  "AVG",
+  "COUNT",
+  "MAX",
+  "MIN",
+  "MEDIAN",
+  "STD",
+];
+
+// FIXME: Operators have a different background.
+// https://github.com/PrismJS/prism/blob/e0ee93f138b7da294a28db50b97c22977fdfc8ed/themes/prism.css#L110
+// https://github.com/PrismJS/prism/pull/2309
+
+const operators = [
+  "AND",
+  "NOT",
+  "OR",
+];
+
+// https://prismjs.com/tokens.html#standard-tokens
+
+Prism.languages["iql"] = {
+  boolean: /\b(?:true|false)\b/,
+  constant: /\bNULL\b/i,
+  number: /\b-?[0-9]+\.?(?:[0-9]+)?\b/,
+  punctuation: /;\s*$/,
+
+  string: {
+    pattern: /\"(?:[^\"]*)+\"/,
+    greedy: true,
+  },
+
+  variable: {
+    pattern: /(VAR\s+)[^0-9\s][\w\-\_\?\.]*/i,
+    lookbehind: true,
+  },
+
+  builtin: new RegExp(`\\b(?:${builtins.join("|")})\\b`, "i"),
+  keyword: new RegExp(`\\b(?:${keywords.join("|")})\\b`, "i"),
+  function: new RegExp(`\\b(?:${functions.join("|")})(?=\\s*\\()`, "i"),
+  operator: new RegExp(`[+\\-*\\/><=/>]|${operators.join("|")}\\b`, "i"),
+};
 
 export const Editor = React.forwardRef(({code, disabled, setCode, ...props }, forwardedRef) => {
   const ref = forwardedRef ?? useRef();
@@ -26,7 +110,7 @@ export const Editor = React.forwardRef(({code, disabled, setCode, ...props }, fo
   }
 
   return (
-    <Highlight {...defaultProps} theme={github} code={code} language="sql">
+    <Highlight {...defaultProps} Prism={Prism} theme={theme} code={code} language="iql">
       {({ className, style, tokens, getTokenProps }) => (
         <Code
           className={className}
