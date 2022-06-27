857fd1de85e6c3b0345b80918a39d13a67860085
Zane Shelby <zane@users.noreply.github.com>
2022-06-27 13:48:58 -0400

    feat(HighlightInput): Add Highlight.js component

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export { default as DataTable } from './DataTable';
 export { default as Editor } from './Editor';
+export { default as HighlightInput } from './HighlightInput';
 export { default as Query } from './Query';
---
+++ stories/HighlightInput.stories.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import HighlightInput from '../src/HighlightInput';
+
+export default {
+  title: 'HighlightInput',
+  component: HighlightInput,
+  argTypes: {},
+};
+
+function Template({ disabled, value }) {
+  const [valueState, setValueState] = useState(value);
+
+  const setValueStateWrapper = (valuee) => {
+    setValueState(valuee);
+  };
+
+  return (
+    <HighlightInput
+      disabled={disabled}
+      value={valueState}
+      onChange={setValueStateWrapper}
+    />
+  );
+}
+
+Template.propTypes = HighlightInput.propTypes;
+Template.defaultProps = HighlightInput.defaultProps;
+
+export const Empty = Template.bind({});
+Empty.args = {
+  value: '',
+};
+
+export const SingleLine = Template.bind({});
+SingleLine.args = {
+  value: 'SELECT * FROM data;',
+};
+
+export const MultiLine = Template.bind({});
+MultiLine.args = {
+  value: 'SELECT *\nFROM data\nWHERE x > 0;',
+};
+
+export const ProbabilityOf = Template.bind({});
+ProbabilityOf.args = {
+  value: `SELECT
+  Period_minutes,
+  Class_of_Orbit,
+
+  PROBABILITY DENSITY OF VAR Period_minutes
+  UNDER satellites_model
+  CONDITIONED BY VAR Class_of_Orbit="GEO"
+FROM satellites`,
+};
