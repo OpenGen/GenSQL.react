afe728b29401bb28bc91e726b480a62f6616626b
Zane Shelby <zane@users.noreply.github.com>
2022-06-30 13:25:11 -0400

    build: Fix build of AMD module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 export { default as DataTable } from './DataTable';
+export { default as GenerationAnimation } from './GenerationAnimation';
 export { default as HighlightInput } from './HighlightInput';
 export { default as PrismInput } from './PrismInput';
 export { default as Query } from './Query';
--- webpack.prod.js
+++ webpack.prod.js
@@ -17,6 +17,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
