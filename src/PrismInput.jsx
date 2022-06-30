92070001c16e97367757c9575ab42d5e6286fda6
Zane Shelby <zane@users.noreply.github.com>
2022-06-30 19:27:49 -0400

    feat(PrismInput): Change to Mantine style

--- src/PrismInput.jsx
+++ src/PrismInput.jsx
@@ -2,9 +2,9 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import Prism from 'prismjs';
 import PropTypes from 'prop-types';
 import React, { useCallback, useRef } from 'react';
-import { Code } from '@mantine/core';
+import { createStyles } from '@mantine/core';
 import { useEditable } from 'use-editable';
-import theme from './themes/nordLight';
+import prismTheme from './themes/nordLight';
 
 // https://prismjs.com/extending.html
 // https://github.com/PrismJS/prism/blob/master/components/prism-sql.js
@@ -78,8 +78,46 @@ Prism.languages.iql = {
   operator: new RegExp(`[+\\-*\\/><=/>]|${operators.join('|')}\\b`, 'i'),
 };
 
+const useStyles = createStyles((theme) => ({
+  pre: {
+    margin: 0,
+  },
+  code: {
+    '&[disabled]': {
+      background: theme.colors.gray[0],
+      cursor: 'not-allowed',
+    },
+    '&:focus-visible': {
+      borderColor: theme.colors.blue[6],
+    },
+    '&:empty': {
+      minHeight: '15px',
+    },
+    borderColor: theme.colors.gray[4],
+    borderRadius: theme.radius.sm,
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    display: 'block',
+    outlineStyle: 'none',
+    overflowX: 'auto',
+    padding: '10px',
+  },
+}));
+
 const PrismInput = React.forwardRef(
-  ({ disabled, indentation, onChange, onKeyDown, value }, forwardedRef) => {
+  (
+    {
+      classNames,
+      className,
+      disabled,
+      indentation,
+      onChange,
+      onKeyDown,
+      styles,
+      value,
+    },
+    forwardedRef
+  ) => {
     const ref = forwardedRef ?? useRef();
 
     const handleChange = useCallback(
@@ -89,6 +127,8 @@ const PrismInput = React.forwardRef(
       },
       [onChange]
     );
+
+    const { classes, cx } = useStyles(undefined, { classNames, styles });
 
     useEditable(ref, handleChange, { disabled, indentation });
 
@@ -104,38 +144,39 @@ const PrismInput = React.forwardRef(
       <Highlight
         {...defaultProps}
         Prism={Prism}
-        theme={theme}
+        theme={prismTheme}
         code={value}
         language="iql"
       >
-        {({ className, style, tokens, getTokenProps }) => (
-          <Code
-            className={className}
-            onKeyDown={handleKeyDown}
-            ref={ref}
-            style={{
-              display: 'block',
-              overflowX: 'auto',
-              padding: '10px',
-              whiteSpace: 'pre',
-              ...style,
-            }}
-          >
-            {tokens.map((line, i) => (
-              // Tokens is static and will not change.
-              /* eslint-disable react/no-array-index-key */
-              <React.Fragment key={i}>
-                {line
-                  .filter((token) => !token.empty)
-                  .map((token, key) => (
-                    // Tokens is static and will not change.
-                    /* eslint-disable react/jsx-key */
-                    <span {...getTokenProps({ token, key })} />
-                  ))}
-                {'\n'}
-              </React.Fragment>
-            ))}
-          </Code>
+        {({ prismClassName, style, tokens, getTokenProps }) => (
+          <pre className={classes.pre}>
+            <code
+              className={cx(classes.code, className, prismClassName)}
+              onKeyDown={handleKeyDown}
+              ref={ref}
+              role="textbox"
+              tabIndex={0}
+              style={{
+                whiteSpace: 'pre',
+                ...style,
+              }}
+            >
+              {tokens.map((line, i) => (
+                // Tokens is static and will not change.
+                /* eslint-disable react/no-array-index-key */
+                <React.Fragment key={i}>
+                  {line
+                    .filter((token) => !token.empty)
+                    .map((token, key) => (
+                      // Tokens is static and will not change.
+                      /* eslint-disable react/jsx-key */
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  {'\n'}
+                </React.Fragment>
+              ))}
+            </code>
+          </pre>
         )}
       </Highlight>
     );
@@ -147,17 +188,23 @@ PrismInput.displayName = 'PrismInput';
 export default PrismInput;
 
 PrismInput.propTypes = {
+  className: PropTypes.string,
+  classNames: PropTypes.object,
   disabled: PropTypes.bool,
   indentation: PropTypes.number,
   onChange: PropTypes.func,
   onKeyDown: PropTypes.func,
+  styles: PropTypes.object,
   value: PropTypes.string,
 };
 
 PrismInput.defaultProps = {
+  className: undefined,
+  classNames: undefined,
   disabled: false,
   indentation: 2,
   onChange: undefined,
   onKeyDown: undefined,
+  styles: undefined,
   value: '',
 };
