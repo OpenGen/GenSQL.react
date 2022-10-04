31a8f3319e1739cf196072ba583f1527aa040fc3
Zane Shelby <zane@users.noreply.github.com>
2022-10-04 19:30:41 -0400

    feat: Add WorldMap component

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,4 @@ export { default as HighlightInput } from './HighlightInput';
 export { default as PrismInput } from './PrismInput';
 export { default as PairPlot } from './PairPlot';
 export { default as Query } from './Query';
+export { default as WorldMap } from './WorldMap';
