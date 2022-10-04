fc078a039409d732d0c6150c426944e38b8eb8db
Zane Shelby <zane@users.noreply.github.com>
2022-10-04 19:25:32 -0400

    test: Add test JSON files, use them in stories

--- stories/DataTable.stories.jsx
+++ stories/DataTable.stories.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import DataTable from '../src/DataTable';
+import cars from './assets/cars.json';
+import penguins from './assets/penguins.json';
 
 export default {
   title: 'DataTable',
@@ -11,15 +13,18 @@ function Template(args) {
   return <DataTable {...args} />;
 }
 
-const columns = ['name', 'age', 'color'];
-const rows = [
-  { name: 'Disco', age: 16, color: 'brown' },
-  { name: 'Henry', age: 14, color: 'orange' },
-  { name: 'Zelda', age: 13, color: 'black' },
+const columns = (data) => [
+  ...new Set(data.flatMap((datum) => Object.keys(datum))),
 ];
 
-export const Basic = Template.bind({});
-Basic.args = {
-  columns,
-  rows,
+export const Cars = Template.bind({});
+Cars.args = {
+  columns: columns(cars),
+  rows: cars,
+};
+
+export const Penguins = Template.bind({});
+Penguins.args = {
+  columns: columns(penguins),
+  rows: penguins,
 };
--- stories/Query.stories.jsx
+++ stories/Query.stories.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Query from '../src/Query';
+import cars from './assets/cars.json';
+import carsTypes from './assets/cars.types.json';
+import penguins from './assets/penguins.json';
+import penguinsTypes from './assets/penguins.types.json';
 
 export default {
   title: 'Query',
@@ -29,4 +33,18 @@ export const Populated = Template.bind({});
 Populated.args = {
   execute,
   initialQuery: 'SELECT * FROM data',
+};
+
+export const Cars = Template.bind({});
+Cars.args = {
+  execute: makeExecute(cars),
+  initialQuery: 'SELECT * FROM cars',
+  statType: (col) => carsTypes[col],
+};
+
+export const Penguins = Template.bind({});
+Penguins.args = {
+  execute: makeExecute(penguins),
+  initialQuery: 'SELECT * FROM penguins',
+  statType: (col) => penguinsTypes[col],
 };
