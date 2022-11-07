4c17411aac1e93e115ec793a8892db676ee55867
joaoloula <jloula@mit.edu>
2022-11-07 14:49:34 -0500

    first draft of gpt-3 integration

--- src/Query.jsx
+++ src/Query.jsx
@@ -7,6 +7,12 @@ import DataTable from './DataTable';
 import HighlightInput from './HighlightInput';
 import PairPlot from './PairPlot';
 import WorldMap from './WorldMap';
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
 
 const useClearableState = (initialValue) => {
   const [value, setValue] = React.useState(initialValue);
@@ -37,8 +43,74 @@ export default function Query({ execute, initialQuery, statType }) {
   const buttonRef = React.useRef();
   const editorRef = React.useRef();
 
+  async function english_to_iql(english_query) {
+    var prompt = `
+### IQL table with properties
+# data(SalaryUSD, Gender, Ethnicity, YearsCodeProfessional, Background)
+# Stackoverflow Developer Survey
+# Explore the developer records
+SELECT * FROM developer_records LIMIT 5
+# Show me developers' salary, gender, and ethnicity
+SELECT SalaryUSD, Gender, Ethnicity FROM developer_records
+# List the 10 most frequent gender and ethnicity pairs
+SELECT
+  COUNT(*) AS n,
+  Gender,
+  Ethnicity
+FROM developer_records
+GROUP BY Gender, Ethnicity
+ORDER BY n DESC
+LIMIT 10
+# Show me the probability of developers' salaries given their gender
+SELECT
+  SalaryUSD,
+  Gender,
+  PROBABILITY OF SalaryUSD
+    UNDER developer_record_generator
+      GIVEN Gender
+        AS probability_salary
+FROM developer_records
+# Show me the probability of developers' salaries given their ethnicity
+SELECT
+  SalaryUSD,
+  Ethnicity,
+  PROBABILITY OF SalaryUSD
+    UNDER developer_record_generator
+      GIVEN Ethnicity
+        AS probability_salary
+FROM developer_records
+# Show me developers gender, ethnicity, and how likely they are to be underpaid based on their experience and background
+  SELECT
+    PROBABILITY OF SalaryUSD >  SalaryUSD
+      UNDER developer_record_generator
+        GIVEN YearsCodeProfessional AND Background
+        AS probability_underpaid,
+    Gender,
+    Ethnicity
+    FROM
+    SELECT * FROM developer_records
+# ${english_query}
+SELECT `;
+    const response = await openai.createCompletion({
+      model: 'text-davinci-002',
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 200,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ['#', ';'],
+    });
+
+    console.log(response.data.choices[0].text);
+    var output = response.data.choices[0].text;
+
+    return 'SELECT ' + output;
+  }
+
   const handleExecute = () => {
-    execute("SELECT " + queryValue)
+    english_to_iql(queryValue)
+      .execute()
       .then(both(setQueryResult, clearErrorValue))
       .catch(both(setErrorValue, clearQueryResult))
       .finally(setNotLoading);
