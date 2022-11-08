3f50814c06906f12d5d0c78d389cfa873bb52937
joaoloula <jloula@mit.edu>
2022-11-08 08:58:23 -0500

    added logging

--- src/Query.jsx
+++ src/Query.jsx
@@ -12,6 +12,7 @@ const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
+console.log(process.env.OPENAI_API_KEY)
 const openai = new OpenAIApi(configuration);
 
 const useClearableState = (initialValue) => {
